44d4c505845238fae455203c762af456b6ec5758
Eda KIRIKKALE <edakirikkale250@gmail.com>
2021-02-15 20:40:38 +0300

    4.Hafta Ödev

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> meyveler = new MyDictionary<string>();
+            meyveler.Add("Muz");
+            meyveler.Add("Erik");
+            meyveler.Add("Çilek");
+
+            Console.WriteLine(meyveler.Count + " elemanlıdır.");
+
+        }
+    }
+}
