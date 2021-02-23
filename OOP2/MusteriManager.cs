bbae763ceb56cac743908fd5749e6559d55cadc7
Eda KIRIKKALE <edakirikkale250@gmail.com>
2021-02-23 18:31:39 +0300

    23 Ocak

---
+++ OOP2/MusteriManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    class MusteriManager
+    {  //iş sınıfları // business classes
+        public void Ekle(Musteri musteri)
+        {
+           
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        private static IKrediManager ihtiyacKrediManager;
+
+        static void Main(string[] args)
+        {
+            IKrediManager ıhtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasıtKrediManager = new TasıtKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(tasıtKrediManager,fileLoggerService);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() { ihtiyacKrediManager,tasıtKrediManager};
+           // basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+        }
+    }
+}
